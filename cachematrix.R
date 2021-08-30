90969d52eaa7f12a03cfd49c758d777d00e63d29
Dattatreya Mitter <amitter7@gmail.com>
2021-08-30 22:56:15 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,35 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        i <- NULL
+  s <- function(y) {
+          x <<- y
+          i <<- NULL
 
 }
+g <- function() x
+  sinverse <- function(inverse) i <<- inverse
+  ginverse <- function() i
+  list(s = s,
+       g = g,
+       sinverse = sinverse,
+       ginverse = ginverse)
+}
 
-
-## Write a short comment describing this function
+## The inverse of the special "matrix" returned by makeCacheMatrix is computed with this function. If the inverse has been calculated before (and the matrix hasn't changed), cacheSolve should get it from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       i <- x$genverse()
+  if (!is.null(i)) {
+          message("Loading the cached data")
+          return(i)
+  }
+  data <- x$g()
+  i <- solve(data, ...)
+  x$sinverse(i)
+  i
 }
+}
+D <- matrix(c(20,42,73,54),2,2)
+D0 <- makeCacheMatrix(D)
+cacheSolve(D0)
